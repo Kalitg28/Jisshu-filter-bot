6c4b0d23107ad4bba376a19f55f0e26f2f485fd1
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-04-29 20:43:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 START_IMG = (environ.get('START_IMG', 'https://envs.sh/6UJ.jpg https://envs.sh/6UJ.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
-PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/6U4.jpg')).split()
+PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/US-.jpg')).split()
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/4J6.jpg'))
 REACTIONS = ["❤️", "😘", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
